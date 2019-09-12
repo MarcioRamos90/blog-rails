e6b50401a586b68902ae836763ba0a113dccf983
Marcio Ramos <marcioramos@alumy.com>
2019-09-12 19:26:42 -0300

    feat: Add destroy actions to article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,15 +3,15 @@ class ArticlesController < ApplicationController
     @articles = Article.all
   end
 
+  def show
+    @article = Article.find(params[:id])
+  end
+
   def new
     @article = Article.new
   end
 
   def edit
-    @article = Article.find(params[:id])
-  end
-
-  def show
     @article = Article.find(params[:id])
   end
 
@@ -33,6 +33,13 @@ class ArticlesController < ApplicationController
     else
       render 'edit'
     end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    redirect_to articles_path
   end
 
   private
